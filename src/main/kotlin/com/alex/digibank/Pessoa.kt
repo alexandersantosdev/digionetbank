7081620d903cc5db2ad5c2aa5706809756b75fe5
alexandersantosdev <lekxandy@hotmail.com>
2021-06-26 16:43:13 -0300

    Mudado a propriedade cpf para set somente lida e não setada

--- src/main/kotlin/com/alex/digibank/Pessoa.kt
+++ src/main/kotlin/com/alex/digibank/Pessoa.kt
@@ -1,18 +1,27 @@
 package com.alex.digibank
 
+//outter class - classe externa
 class Pessoa {
     var nome: String = "Alexander"
-    val cpf: String = "111.111.111-11"
+    var cpf: String = "111.111.111-11"
+    private set
 
+    //classe interna
+    /*
     inner class Endereco{
         var rua:String = "Rua de Teste"
     }
+     */
 }
 
 fun main() {
+
     val alex = Pessoa()
+
+    println(alex)
     println(alex.nome)
     println(alex.cpf)
-    println(alex.Endereco().rua)
+
+    //println(alex.Endereco().rua)
 
 }
