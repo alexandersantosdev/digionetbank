0a06bfebe99593295ca9d13e16fb2e09d9dda9af
alexandersantosdev <lekxandy@hotmail.com>
2021-06-26 16:27:01 -0300

    Created Pessoa class

--- src/main/kotlin/com/alex/digibank/Pessoa.kt
+++ src/main/kotlin/com/alex/digibank/Pessoa.kt
@@ -3,10 +3,15 @@ package com.alex.digibank
 class Pessoa {
     var nome: String = "Alexander"
     val cpf: String = "055.813.279-04"
+
+    inner class Endereco{
+        var rua:String = "Antonio Valenga"
+    }
 }
 
 fun main() {
     val alex = Pessoa()
     println(alex.nome)
     println(alex.cpf)
+    println(alex.Endereco().rua)
 }
