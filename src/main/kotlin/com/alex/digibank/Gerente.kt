1f5691cffeff15a7d822ad70fcd886e894d6a94f
Alexander Roberto dos Santos <84203330+alexandersantosdev@users.noreply.github.com>
2021-06-26 21:31:34 -0300

    Update Gerente.kt

--- src/main/kotlin/com/alex/digibank/Gerente.kt
+++ src/main/kotlin/com/alex/digibank/Gerente.kt
@@ -1,8 +1,6 @@
 package com.alex.digibank
-interface Teste{
 
-}
-class Gerente(nome:String, cpf:String, salario:Double, val senha:String):Funcionario(nome, cpf, salario), Logavel, Teste {
+class Gerente(nome:String, cpf:String, salario:Double, val senha:String):Funcionario(nome, cpf, salario), Logavel {
     override fun calcBeneficio() = salario * 0.3
     override fun login(): Boolean = "senha123" == senha
 }
